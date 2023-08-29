2568d9f1b22b4a285c6ffce9bc4355ab647b0f92
Stefan Heid <stefan.heid@upb.de>
2023-08-29 17:52:42 +0200

    significantly simplified weighter function

--- gamma_correlation/distances.py
+++ gamma_correlation/distances.py
@@ -1,43 +1,27 @@
-import numpy as np
+def d_max(data, i, j, column):
+    """Pseudo-metric according to eq (11) in the paper
 
-
-def d_sum(x: int, y: int, weights: np.ndarray):
+    :param data: Rank data
+    :param i: Rank index i
+    :param j: Rank index j
+    :param column: column index
+    :return: Value of pseudo-metric as a global distance funcion
     """
-    Pseudo-metric according to eq (15)
+    return max(data[int(min(data[i, column], data[j, column])) - 1:
+                    int(max(data[i, column], data[j, column])) - 1,
+               -1])
 
-    :param x: rank of first elem (one-indexed)
-    :param y: rank of second elem (one indexed)
-    :param weights: distance weight
-    :return:
+
+def d_sum(data, i, j, column):
+    """Pseudo-metric according to eq (12) in the paper
+
+    :param data: Rank data
+    :param i: Rank index i
+    :param j: Rank index j
+    :param column: Column index
+    :return: Value of pseudo-metric as a global distance funcion on
     """
-    lo, hi = np.sort([x, y]) - 1
-    return min(1, weights[lo:hi].sum())
-
-
-def d_max(x: int, y: int, weights: np.ndarray):
-    """
-    Pseudo-metric according to eq (16)
-
-    :param x: rank of first elem (one-indexed)
-    :param y: rank of second elem (one-indexed)
-    :param weights: distance weight
-    :return:
-    """
-    lo, hi = np.sort([x, y]) - 1
-    return weights[lo:hi].max()
-
-
-def R(x: int, y: int, d: callable, weights: np.ndarray):
-    """
-    Antisymmetric version of the distance measure, basically clamping values to 0 when the inputs are inversely ordered
-
-    :param x: rank of first elem (one-indexed)
-    :param y: rank of second elem (one-indexed)
-    :param d: Distance function
-    :param weights: distance weight
-    :return: value of the fuzzy order relation R as defined in [1]
-    """
-    if x < y:
-        return d(x, y, weights)
-    else:
-        return 0
+    return min(1,
+               sum(data[int(min(data[i, column], data[j, column])) - 1:
+                        int(max(data[i, column], data[j, column])) - 1,
+                   -1]))
