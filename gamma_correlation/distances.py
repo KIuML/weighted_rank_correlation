8802046539cc787a2b88d34c307f3b83d5895d92
Stefan Heid <stefan.heid@upb.de>
2023-09-06 17:56:41 +0200

    vectorzed distance functions and removed R function

--- gamma_correlation/distances.py
+++ gamma_correlation/distances.py
@@ -1,43 +1,25 @@
 import numpy as np
 
 
-def d_sum(x: int, y: int, weights: np.ndarray):
+def d_sum(pair: np.array, weights: np.array):
     """
-    Pseudo-metric according to eq (15)
+    Sum of weights between lower and higher rank clamped to the [0,1] interval.
 
-    :param x: rank of first elem (one-indexed)
-    :param y: rank of second elem (one indexed)
+    :param x: 1-D array of ranks of first elem (one-indexed)
+    :param y: 1-D array of ranks of second elem (one indexed)
     :param weights: distance weight
     :return:
     """
-    lo, hi = np.sort([x, y]) - 1
-    return min(1, weights[lo:hi].sum())
+    return np.apply_along_axis(lambda idx: np.minimum(weights[slice(*idx)].sum(), 1), 0, np.array(pair) - 1)
 
 
-def d_max(x: int, y: int, weights: np.ndarray):
+def d_max(pair: np.array, weights: np.ndarray):
     """
-    Pseudo-metric according to eq (16)
+    Maximum weight between upper and lower rank.
 
     :param x: rank of first elem (one-indexed)
     :param y: rank of second elem (one-indexed)
     :param weights: distance weight
     :return:
     """
-    lo, hi = np.sort([x, y]) - 1
-    return weights[lo:hi].max()
-
-
-def R(x: int, y: int, d: callable, weights: np.ndarray):
-    """
-    Antisymmetric version of the distance measure, basically clamping values to 0 when the inputs are inversely ordered
-
-    :param x: rank of first elem (one-indexed)
-    :param y: rank of second elem (one-indexed)
-    :param d: Distance function
-    :param weights: distance weight
-    :return: value of the fuzzy order relation R as defined in [1]
-    """
-    if x < y:
-        return d(x, y, weights)
-    else:
-        return 0
+    return np.apply_along_axis(lambda idx: weights[slice(*idx)].max(), 0, np.array(pair) - 1)
--- gamma_correlation/main.py
+++ gamma_correlation/main.py
@@ -3,9 +3,9 @@ from typing import Union
 
 import numpy as np
 
-from gamma_correlation.util import gen_weights
-from gamma_correlation.distances import d_max, R
+from gamma_correlation.distances import d_max
 from gamma_correlation.tnorms import prod
+from gamma_correlation.util import gen_weights
 
 
 def gamma_corr(ranking_a: Union[list, np.ndarray], ranking_b: Union[list, np.ndarray], *,
@@ -18,8 +18,8 @@ def gamma_corr(ranking_a: Union[list, np.ndarray], ranking_b: Union[list, np.nda
 
     con = dis = 0
     for i, j in combinations(range(n), 2):
-        a_ij, b_ij = [R(r[i], r[j], d, weights) for r in [ranking_a, ranking_b]]
-        a_ji, b_ji = [R(r[j], r[i], d, weights) for r in [ranking_a, ranking_b]]
+        a_ij, b_ij = [d([[r[i]], [r[j]]], weights) if r[i] < r[j] else 0 for r in [ranking_a, ranking_b]]
+        a_ji, b_ji = [d([[r[j]], [r[i]]], weights) if r[j] < r[i] else 0 for r in [ranking_a, ranking_b]]
 
         con += tnorm(a_ij, b_ij) + tnorm(a_ji, b_ji)
         dis += tnorm(a_ij, b_ji) + tnorm(a_ji, b_ij)
